494838af34ab1d5a12a0dcad1c5abb74d7aa90dd
daisuke33 <67767613+ukigumo-shiina3@users.noreply.github.com>
2021-08-07 14:44:44 +0900

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFrag, setfaceShowFrag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setfaceShowFrag(!faceShowFrag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFrag || setfaceShowFrag(true);
+      } else {
+        faceShowFrag && setfaceShowFrag(false);
+      }
+    }
+    //eslint disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFrag && <p>( ꒪Д꒪)</p>}
     </>
   );
 };
